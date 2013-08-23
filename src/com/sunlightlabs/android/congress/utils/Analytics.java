6f3a8e50059c9bc7093ec417d161a4e360f4bc9d
Eric Mill <eric@konklone.com>
2013-08-22 20:49:21 -0400

    Cleaning up after refactor, removed unneeded code, refactored tracker out of the Footer class

--- src/com/sunlightlabs/android/congress/utils/Analytics.java
+++ src/com/sunlightlabs/android/congress/utils/Analytics.java
@@ -21,12 +21,8 @@ public class Analytics {
 	//TODO:
 	//  * one-off URLs -> events (e.g. about)
 	//  * remove tracker object from Footer altogether?
-	//  * remove tracker from event calls, look up tracker as needed from fragment
-	
-	// scope constants as used in Google Analytics
-//	public static final int SCOPE_VISITOR = 1;
-//	public static final int SCOPE_SESSION = 2;
-//	public static final int SCOPE_PAGE = 3;
+	//  * custom variables on events
+	//  * is entry tracking impossible with EasyTracker?
 	
 	// custom variable slots (can range from 1-5)
 	public static final int CUSTOM_VERSION_SLOT = 2;
@@ -74,72 +70,6 @@ public class Analytics {
 	public static final String BILL_UPCOMING_MORE = "upcoming_more";
 	public static final String ANALYTICS_DISABLE = "disable";
 	
-//	private static final String FRAGMENT_TAG = "com.sunlightlabs.android.congress.utils.Analytics.analytics";
-	
-	// wrapper fragment for a GoogleAnalyticsTracker, manages its own state across activity restarts
-//	public static class TrackerFragment extends Fragment {
-//		public EasyTracker tracker;
-//		
-//		@Override
-//		public void onDetach() {
-//			super.onDetach();
-//			Analytics.stop(tracker);
-//		}
-//		
-//		public TrackerFragment() {}
-//	}
-	
-	// simple convenience function for when a fragment wants to log a URL
-	// don't need to worry about making sure it only happens once, because Fragments manage their own state
-	// in a way that activities do not.
-	// TODO: is this needed?
-//	public static void track(Fragment fragment, String url) {
-//		if (fragment.isAdded()) {
-//			GoogleAnalyticsTracker tracker = start(fragment.getActivity());
-//			page(fragment.getActivity(), tracker, url);
-//			stop(tracker);
-//		}
-//	}
-	
-	// Convenience function.
-	// Will take care of logging the pageview, and then *not* logging it if the activity restarts because of a screen flip.
-	// Uses a fragment's presence as a marker of whether the initial pageview has been tracked already,
-	// and uses it as a holder for the tracker itself, returning it and replacing it when the activity changes.
-	// Finally, also uses its lifecycle as a way to automatically stop the tracker.
-	
-//	public static TrackerFragment track(FragmentActivity activity, String url) {
-//		if (analyticsEnabled(activity)) {
-//			
-//			// I initialize the tracker here and call start(), instead of inside the fragment using callbacks,
-//			// so that we can log the inaugural pageview here, where we know whether it's the first time it happened
-//			FragmentManager manager = activity.getSupportFragmentManager();
-//			TrackerFragment fragment = (TrackerFragment) manager.findFragmentByTag(FRAGMENT_TAG);
-//			if (fragment == null) {
-//				fragment = new TrackerFragment();
-//				fragment.setRetainInstance(true);
-//				manager.beginTransaction().add(fragment, FRAGMENT_TAG).commit();
-//				
-//				// initialize the tracker immediately, so it can be used by the caller
-//				fragment.tracker = start(activity);
-//				
-//				// if this was used as a convenience function in an activity, 
-//				// log the pageview only once, when the fragment is first created
-//				//TODO: this can safely be removed, we don't do URLs now
-////				if (url != null)
-////					page(activity, fragment.tracker, url);
-//			} 
-//			
-//			// if this was used as a convenience function in an activity, reassign and restart 
-//			// the tracker since we'll assume that this is the first call after the activity was restarted
-//			else if (url != null) 
-//				fragment.tracker = start(activity);
-//			
-//			// otherwise, leave the fragment alone, nothing to do here
-//			
-//			return fragment;
-//		} else
-//			return null;
-//	}
 	 
 	public static EasyTracker trackerFor(Activity activity) {
 		return EasyTracker.getInstance(activity);
@@ -151,57 +81,16 @@ public class Analytics {
 			Log.i(Utils.TAG, "[Analytics] Tracker starting for " + activity.getLocalClassName());
 			EasyTracker tracker = EasyTracker.getInstance(activity);
 			tracker.activityStart(activity);
-//			GoogleAnalyticsTracker tracker = GoogleAnalyticsTracker.getInstance();
-//			String code = activity.getResources().getString(R.string.ga_trackingId);
-//			tracker.startNewSession(code, activity);
 			return tracker;
 		} else
 			return null;
 	}
 	
 	
-//	public static void page(FragmentActivity activity, String page) {
-//		page(activity, page, true);
-//	}
-//	
-//	public static void page(FragmentActivity activity, String page, boolean checkEntry) {
-//		page(activity, trackerFor(activity), page, checkEntry);
-//	}
-//	
-//	public static void page(Activity activity, EasyTracker tracker, String page) {
-//		page(activity, tracker, page, true);
-//	}
-//	
-//	public static void page(Activity activity, EasyTracker tracker, String page, boolean checkEntry) {
-//		if (tracker != null && analyticsEnabled(activity)) {
-//			
-//			if (checkEntry) {
-//				String source = entrySource(activity);
-//				if (source != null) {
-//					Log.i(Utils.TAG, "[Analytics] Marking next page view as an entry to the app of type: " + source);
-//					
-//					markEntry(activity, tracker, source);
-//				}
-//			}
-//			
-//			// TODO: custom vars to hits
-////			tracker = attachCustomVars(activity, tracker);
-//			
-//			Log.i(Utils.TAG, "[Analytics] Tracking page - " + page);
-//			//tracker.setDebug(true);
-//			tracker.setAnonymizeIp(true);
-//			tracker.trackPageView(page);
-//			tracker.dispatch();
-//		}
-//	}
-		
 	public static void event(Activity activity, String category, String action, String label) {
-		event(activity, trackerFor(activity), category, action, label);
-	}
-	
-	public static void event(Activity activity, EasyTracker tracker, String category, String action, String label) {
+		EasyTracker tracker = trackerFor(activity);
 		if (tracker != null && analyticsEnabled(activity)) {
-			// TODO: custom variables on events
+
 //			tracker = attachCustomVars(activity, tracker);			
 			Log.i(Utils.TAG, "[Analytics] Tracking event - category: " + category + ", action: " + action + ", label: " + label);
 			
@@ -237,27 +126,27 @@ public class Analytics {
 	}
 	
 	public static void addFavoriteLegislator(Activity activity, String bioguideId) {
-		event(activity, trackerFor(activity), EVENT_FAVORITE, FAVORITE_ADD_LEGISLATOR, bioguideId);
+		event(activity, EVENT_FAVORITE, FAVORITE_ADD_LEGISLATOR, bioguideId);
 	}
 	
 	public static void removeFavoriteLegislator(Activity activity, String bioguideId) {
-		event(activity, trackerFor(activity), EVENT_FAVORITE, FAVORITE_REMOVE_LEGISLATOR, bioguideId);
+		event(activity, EVENT_FAVORITE, FAVORITE_REMOVE_LEGISLATOR, bioguideId);
 	}
 	
 	public static void addFavoriteBill(Activity activity, String billId) {
-		event(activity, trackerFor(activity), EVENT_FAVORITE, FAVORITE_ADD_BILL, billId);
+		event(activity, EVENT_FAVORITE, FAVORITE_ADD_BILL, billId);
 	}
 	
 	public static void removeFavoriteBill(Activity activity, String billId) {
-		event(activity, trackerFor(activity), EVENT_FAVORITE, FAVORITE_REMOVE_BILL, billId);
+		event(activity, EVENT_FAVORITE, FAVORITE_REMOVE_BILL, billId);
 	}
 	
-	public static void subscribeNotification(Activity activity, EasyTracker tracker, String subscriber) {
-		event(activity, tracker, EVENT_NOTIFICATION, NOTIFICATION_ADD, subscriber);
+	public static void subscribeNotification(Activity activity, String subscriber) {
+		event(activity, EVENT_NOTIFICATION, NOTIFICATION_ADD, subscriber);
 	}
 	
-	public static void unsubscribeNotification(Activity activity, EasyTracker tracker, String subscriber) {
-		event(activity, tracker, EVENT_NOTIFICATION, NOTIFICATION_REMOVE, subscriber);
+	public static void unsubscribeNotification(Activity activity, String subscriber) {
+		event(activity, EVENT_NOTIFICATION, NOTIFICATION_REMOVE, subscriber);
 	}
 	
 	public static void legislatorCall(Activity activity, String bioguideId) {
@@ -277,35 +166,35 @@ public class Analytics {
 	}
 	
 	public static void billShare(Activity activity, String billId) {
-		event(activity, trackerFor(activity), EVENT_BILL, BILL_SHARE, billId);
+		event(activity, EVENT_BILL, BILL_SHARE, billId);
 	}
 	
 	public static void billText(Activity activity, String billId) {
-		event(activity, trackerFor(activity), EVENT_BILL, BILL_TEXT, billId);
+		event(activity, EVENT_BILL, BILL_TEXT, billId);
 	}
 	
 	public static void billOpenCongress(Activity activity, String billId) {
-		event(activity, trackerFor(activity), EVENT_BILL, BILL_OPENCONGRESS, billId);
+		event(activity, EVENT_BILL, BILL_OPENCONGRESS, billId);
 	}
 	
 	public static void billGovTrack(Activity activity, String billId) {
-		event(activity, trackerFor(activity), EVENT_BILL, BILL_GOVTRACK, billId);
+		event(activity, EVENT_BILL, BILL_GOVTRACK, billId);
 	}
 	
 	public static void billUpcomingMore(Activity activity, String sourceType) {
-		event(activity, trackerFor(activity), EVENT_BILL, BILL_UPCOMING_MORE, sourceType);
+		event(activity, EVENT_BILL, BILL_UPCOMING_MORE, sourceType);
 	}
 	
 	public static void billUpcoming(Activity activity, String billId) {
-		event(activity, trackerFor(activity), EVENT_BILL, BILL_UPCOMING, billId);
+		event(activity, EVENT_BILL, BILL_UPCOMING, billId);
 	}
 	
-	public static void markEntry(Activity activity, EasyTracker tracker, String source) {
-		event(activity, tracker, EVENT_ENTRY, source, source);
+	public static void markEntry(Activity activity, String source) {
+		event(activity, EVENT_ENTRY, source, source);
 	}
 	
 	public static void analyticsDisable(Activity activity) {
-		event(activity, trackerFor(activity), EVENT_ANALYTICS, ANALYTICS_DISABLE, "");
+		event(activity, EVENT_ANALYTICS, ANALYTICS_DISABLE, "");
 	}
 	
 	/** Utility function for discerning an entry source from an activity's Intent. */ 
@@ -326,7 +215,6 @@ public class Analytics {
 //			return null;
 //	}
 	
-	// TODO: I think this is dead, with EasyTracker?
 	public static Intent passEntry(Activity activity, Intent intent) {
 		String action = activity.getIntent().getAction();
 		if (action != null && action.equals(Intent.ACTION_MAIN)) {
